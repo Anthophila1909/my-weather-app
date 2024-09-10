e51f73d101adcd14e7c4707e45a638d27214f9b9
Anthophila1909 <elena.w@hotmail.de>
2024-09-10 20:28:53 +0200

    JS: Added search functionality using API
    In order to change h1 and its current temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city-input");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "93tdd4d3b4c2db3bcc87b00foc83ce4a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function enterCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city-input");
-  city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormInput = document.querySelector("#search-form");
 searchFormInput.addEventListener("submit", enterCity);
+
+searchCity("Ulm");
